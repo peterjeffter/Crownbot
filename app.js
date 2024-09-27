fdf9313d3e468ac918dce6a33b5c759c5b9d9bbc
peterjeffter <159525761+peterjeffter@users.noreply.github.com>
2024-09-27 19:11:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
+// Initialize Telegram WebApp
+Telegram.WebApp.ready();
+
+// Function to update the navigation button based on the current page
+function updateNavigationButton() {
+  const isIndexPage = window.location.pathname === '/index.html'; // Check if we're on the 
+  const isTaskPage = window.location.pathname === '/tasks.html';
+  const isFriendsPage = window.location.pathname === '/friends.html';
+  const isAirdropPage = window.location.pathname === '/airdrop.html';
+  const isProfPage = window.location.pathname === '/profilesettings'
+
+  
+  if (isIndexPage) {
+    // Hide the "Back" button on the homepage (default Close button will appear)
+    Telegram.WebApp.BackButton.hide();
+  } else if( isProfPage || isTaskPage || isFriendsPage || isAirdropPage){
+    // Show the "Back" button on all other pages
+    Telegram.WebApp.BackButton.show();
+    Telegram.WebApp.BackButton.onClick(() => {
+      window.history.back(); // Navigate to the previous page
+    });
+  }
+}
+
+// Call the function initially to set up the button
+updateNavigationButton();
+
+// Listen for changes in navigation (if the user uses the browser's back/forward buttons)
+window.addEventListener('popstate', updateNavigationButton);
 

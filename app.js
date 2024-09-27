212a333c106be5d60f6fb0294d2743df4937a528
peterjeffter <159525761+peterjeffter@users.noreply.github.com>
2024-09-27 08:52:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,11 +7,16 @@ function updateNavigationButton() {
   const isTaskPage = window.location.pathname === '/tasks.html';
   const isFriendsPage = window.location.pathname === '/friends.html';
   const isAirdropPage = window.location.pathname === '/airdrop.html';
-  if (isProfPage, isTaskPage, isFriendsPage, isAirdropPage) {
+  
+  // Show the "Back" button if on any page except the homepage
+  if (isProfPage || isTaskPage || isFriendsPage || isAirdropPage) {
     Telegram.WebApp.BackButton.show(); // Show the "Back" button
     Telegram.WebApp.BackButton.onClick(() => {
       window.history.back(); // Navigate to the previous page
     });
+  } else {
+    // Hide the "Back" button on other pages
+    Telegram.WebApp.BackButton.hide(); // This can be used if you want to revert to the default close button.
   }
 }
 

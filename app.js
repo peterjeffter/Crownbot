054bd6a7a865dfe52164b1a86d7a194fcbf2a5e4
peterjeffter <159525761+peterjeffter@users.noreply.github.com>
2024-09-27 08:51:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ Telegram.WebApp.ready();
 
 // Function to update the navigation button based on the current page
 function updateNavigationButton() {
-  const isIndexPage = window.location.pathname === '/index.html'; // Adjust the path if your homepage has a different filename
-
-  if (isIndexPage) {
-    // Show the default "Close" button on the homepage
-    Telegram.WebApp.BackButton.hide(); // Hide the "Back" button
-  } else {
-    // Show the "Back" button on any other page
+  const isProfPage = window.location.pathname === '/index.html'; // Adjust the path if your homepage has a different filename
+  const isTaskPage = window.location.pathname === '/tasks.html';
+  const isFriendsPage = window.location.pathname === '/friends.html';
+  const isAirdropPage = window.location.pathname === '/airdrop.html';
+  if (isProfPage, isTaskPage, isFriendsPage, isAirdropPage) {
     Telegram.WebApp.BackButton.show(); // Show the "Back" button
     Telegram.WebApp.BackButton.onClick(() => {
       window.history.back(); // Navigate to the previous page
